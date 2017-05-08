7932e03faebdb8172edf45d45ee088bd62581e70
Simon Hansson <Simon@Simons-MacBook-Pro.local>
2017-05-08 16:47:59 +0200

    First commit

---
+++ Pendulum.m
@@ -0,0 +1,7 @@
+%----------Model Constants------------
+M = 0.5;    % Cart Mass (kg)
+m = 0.4;    % Pendulum Mass (kg)
+b = 0.1;    % Coefficient of Friction for Cart (N/m/sec) 
+I = 0.006;  % Mass Moment of Inertia of the Pendulum (kg m^2)
+g = 9.8;    % Gravity (N)
+l = 0.5;    % Pendulum Length (m)
